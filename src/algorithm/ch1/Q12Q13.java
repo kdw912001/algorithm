d9df12aa45d9b678c384b55dd71e628b2d0817a1
kdw912001 <kdw912001@naver.com>
2018-12-16 01:23:57 +0900

    Initial commit

---
+++ src/algorithm/ch1/Q12Q13.java
@@ -0,0 +1,107 @@
+package algorithm.ch1;
+
+public class Q12Q13 {
+
+	public static void q12() {
+		/*System.out.print(" | ");
+		for(int i=1; i<=9;i++) {
+			System.out.print(i+"  ");
+		}
+		System.out.println();
+		System.out.println("-+--------------------------");
+		for(int i=1; i<=9; i++) {
+			System.out.print(i+"| ");
+			for(int j=1; j<=9;j++){
+				
+				System.out.print(i*j+" ");
+				if(i*j<10) {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}*/
+		
+		//
+		for(int i=-1;i<=9;i++) {
+			for(int j=-1;j<=9;j++) {
+				if(i==-1) {
+					if(j==-1)
+						System.out.print("   ");
+					else if(j>=1)
+						System.out.print(j+"  ");
+				}
+				if(i==0) {
+					if(j==0){
+						System.out.print("+");
+					}
+					else
+						System.out.print("---");
+				}
+				if(j==-1) {
+					if(i>=1)
+						System.out.print(i+"  ");
+				}
+				if(j==0) {
+					if(i==0) {
+						//
+					}else {
+						System.out.print("| ");
+					}
+				}
+				
+				if(i>=1 &&j>=1)	{
+					System.out.print(i*j+" ");
+					if(i*j<10)
+						System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+}
+
+public static void q13() {
+	//
+	for(int i=-1;i<=9;i++) {
+		for(int j=-1;j<=9;j++) {
+			if(i==-1) {
+				if(j==-1)
+					System.out.print("   ");
+				else if(j>=1)
+					System.out.print(j+"  ");
+			}
+			if(i==0) {
+				if(j==0){
+					System.out.print("+");
+				}
+				else
+					System.out.print("---");
+			}
+			if(j==-1) {
+				if(i>=1)
+					System.out.print(i+"  ");
+			}
+			if(j==0) {
+				if(i==0) {
+					//
+				}else {
+					System.out.print("| ");
+				}
+			}
+			
+			if(i>=1 &&j>=1)	{
+				System.out.print((i+j)+" ");
+				if(i+j<10)
+					System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+}
+
+public static void main(String[] args) {
+	q12();
+	//q13();
+
+}
+
+}
---
+++ src/algorithm/ch1/Q14Q15.java
@@ -0,0 +1,70 @@
+package algorithm.ch1;
+
+import java.util.Scanner;
+
+public class Q14Q15 {
+
+	private static Scanner sc = new Scanner(System.in); 
+	public void q14() {
+		System.out.println("");
+		System.out.print(" "); 
+		 
+		int dan = sc.nextInt();
+		for(int i=0;i<dan;i++) {
+			for(int j=0; j<dan;j++) {
+				System.out.print("*");
+			}  
+			System.out.println();  
+		}
+	}
+	public static void triangleLB(int n) {
+		for(int i=0; i<n;i++) {
+			for(int j=0; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void triangleLU(int n) {
+		for(int i=0; i<n;i++) {
+			for(int j=i; j<n;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void triangleRU(int n) {
+		for(int i=0; i<n;i++) {
+			for(int j=0; j<n;j++) {
+				if(i>j)
+					System.out.print(" ");
+				else
+					System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void triangleRB(int n) {
+		for(int i=0; i<n;i++) {
+			for(int j=0; j<n-i-1;j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		/*Second se = new Second();
+		se.q14();*/
+		System.out.print(" ");
+		int n = sc.nextInt();
+		triangleLB(n);
+		triangleLU(n);
+		triangleRU(n);
+		triangleRB(n);
+		
+	}
+
+}
